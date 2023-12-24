f7d170adb0a2869827c66a328dbc2a5c55618c5b
JhostinJATM <jhos_tap@hotmail.com>
2023-12-24 14:25:37 -0500

    Clase Materia
    Atributos, constructores, getters y setters

--- src/modelo/Materia.java
+++ src/modelo/Materia.java
@@ -10,4 +10,43 @@ package modelo;
  */
 public class Materia {
     
+    private Integer id;
+    private String nombreMateria;
+    private String descripcion;
+
+    public Materia() {
+    }
+
+    public Materia(Integer id, String nombreMateria, String descripcion) {
+        this.id = id;
+        this.nombreMateria = nombreMateria;
+        this.descripcion = descripcion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombreMateria() {
+        return nombreMateria;
+    }
+
+    public void setNombreMateria(String nombreMateria) {
+        this.nombreMateria = nombreMateria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
 }
