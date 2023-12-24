35aa2abf40ff509b9193161b0c18d6c07793130b
Brian Fernando <brian.aguinsaca@unl.edu.ec>
2023-12-23 22:26:32 -0500

    Agregando Atributos. Constructores, Getter, Setter y Métodos de la clase Docente

--- src/modelo/Docente.java
+++ src/modelo/Docente.java
@@ -4,10 +4,49 @@
  */
 package modelo;
 
+import modelo.enums.Genero;
+
 /**
  *
  * @author Usuario iTC
  */
-public class Docente {
+public class Docente extends Persona{
+    
+    //Atributos
+    private String titulo;
+    
+    
+    //Constructores
+    public Docente() {
+    }
+
+    public Docente(String titulo, Integer id, String nombres, String apellidos, Integer edad, Genero genero, String direccion, String telefono, String cedula) {
+        super(id, nombres, apellidos, edad, genero, direccion, telefono, cedula);
+        this.titulo = titulo;
+    }
+    
+    //Getter and Setter
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    
+    //Print
+
+    @Override
+    public String toString() {
+        return "Docente{" + "titulo=" + titulo + "}" + super.toString();
+    }
+    
+    public static void main (String[] args){
+        Docente d = new Docente();
+        d.setTitulo("Maestria");
+        System.out.println(d.toString());
+    }
+    
     
 }
