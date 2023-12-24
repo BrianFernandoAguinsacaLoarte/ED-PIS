18946f98abf62f992ecfe157caa655dee9dba81b
JhostinJATM <jhos_tap@hotmail.com>
2023-12-24 13:27:15 -0500

    Clase Tarea
    Atributos, constructores, setters y getters

--- src/modelo/Tarea.java
+++ src/modelo/Tarea.java
@@ -4,10 +4,69 @@
  */
 package modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author Usuario iTC
  */
 public class Tarea {
+    
     private Integer id;
+    private String tema;
+    private Date fechaCreacion;
+    private Date fechaEntrega;
+    private String archivo;
+
+    public Tarea() {
+    }
+
+    public Tarea(Integer id, String tema, Date fechaCreacion, Date fechaEntrega, String archivo) {
+        this.id = id;
+        this.tema = tema;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaEntrega = fechaEntrega;
+        this.archivo = archivo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(Date fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public String getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(String archivo) {
+        this.archivo = archivo;
+    }
+    
 }
