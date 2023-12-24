8ada9faffcca2c62250577f5416497adeae65769
Brian Fernando <brian.aguinsaca@unl.edu.ec>
2023-12-23 22:19:43 -0500

    Agregando Atributos, Constructores, Getter, Setter y Métodos a la clase Estudiante

--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -4,10 +4,57 @@
  */
 package modelo;
 
+import modelo.enums.Genero;
+
 /**
  *
  * @author Usuario iTC
  */
-public class Estudiante {
+public class Estudiante extends Persona {
+    
+    //Atributos
+    private String colegioAnterior;
+    
+    
+    //Constructores
+    public Estudiante() {
+    }
+    
+    public Estudiante(Integer id, String nombres, String apellidos, Integer edad, Genero genero, String direccion, String telefono, String cedula, String colegioAnterior) {
+        super(id, nombres, apellidos, edad, genero, direccion, telefono, cedula);
+        this.colegioAnterior = colegioAnterior; 
+    }
+    
+    //Getter and Setter
+
+    public String getColegioAnterior() {
+        return colegioAnterior;
+    }
+
+    public void setColegioAnterior(String colegioAnterior) {
+        this.colegioAnterior = colegioAnterior;
+    }
+
+    //Print
+    @Override
+    public String toString() {
+        return "Estudiante{" + "colegioAnterior=" + colegioAnterior + "}" + super.toString();
+    }
+    
+    
+    //Main
+    public static void main (String[] args){
+        Estudiante e = new Estudiante();
+        
+        e.setColegioAnterior("La Dolorosa");
+        System.out.println(e.toString());
+    }
+    
+    
+    
+    
+
+    
+    
     
 }
