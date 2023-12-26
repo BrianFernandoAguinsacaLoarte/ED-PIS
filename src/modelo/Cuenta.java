28666098212bfdc7001966e420db8027090b368b
Santiago2k4 <ordonezsantiago2k4@gmail.com>
2023-12-25 22:16:21 -0500

    atributos de cuenta

--- src/modelo/Cuenta.java
+++ src/modelo/Cuenta.java
@@ -10,4 +10,26 @@ package modelo;
  */
 public class Cuenta {
     private String usuario;
+    private String contraseña;
+
+    public Cuenta(String usuario, String contraseña) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }  
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
 }
