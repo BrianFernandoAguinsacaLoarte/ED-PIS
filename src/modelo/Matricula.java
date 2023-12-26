d30a07e993b1c43a6839d7ca7797227ae5ce5848
Santiago2k4 <ordonezsantiago2k4@gmail.com>
2023-12-25 23:50:59 -0500

    atributos Matricula

--- src/modelo/Matricula.java
+++ src/modelo/Matricula.java
@@ -4,10 +4,61 @@
  */
 package modelo;
 
+import java.util.Date;
+
+
+
 /**
  *
  * @author Usuario iTC
  */
 public class Matricula {
+    private Integer id;
+    private String estado;
+    private Estudiante estudiante;
+    private Materia materia;
+    private Curso curso;
+    private PeriodoAcademico p_academico;
+    private Date fechaMatricula;
+
+    public Matricula(Estudiante estudiante, Curso curso, Date fechaMatricula) {
+        this.estudiante = estudiante;
+        this.curso = curso;
+        this.fechaMatricula = fechaMatricula;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Date getFechaMatricula() {
+        return fechaMatricula;
+    }
+
+    public void setFechaMatricula(Date fechaMatricula) {
+        this.fechaMatricula = fechaMatricula;
+    }
+
     
+
+    @Override
+    public String toString() {
+        return "Matricula{" +
+                "estudiante=" + estudiante +
+                ", curso=" + curso +
+                ", fechaMatricula=" + fechaMatricula +
+                '}';
+    }
 }
