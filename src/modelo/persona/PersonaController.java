b939e8811be02c6a5662151977295cc27a88ad5c
Brian Fernando <brian.aguinsaca@unl.edu.ec>
2023-12-26 19:36:40 -0500

    Creación y agregando métodos al PersonaController

--- src/modelo/persona/PersonaController.java
+++ src/modelo/persona/PersonaController.java
@@ -4,10 +4,57 @@
  */
 package modelo.persona;
 
+import modelo.Persona;
+import controlador.TDA.listas.LinkedList;
+import controlador.dao.DataAccessObject;
+
 /**
  *
  * @author Usuario iTC
  */
-public class PersonaController {
+public class PersonaController extends DataAccessObject<Persona> {
+    
+    //Objetos 
+    private Persona persona = new Persona();
+    private LinkedList<Persona> personas = new LinkedList<>();
+
+    
+    //Constructor
+    public PersonaController() {
+        super(Persona.class);
+    }
+
+    //Metodos 
+     public boolean save(){
+        persona.setId(generated_id());//BDD Esto desaparece
+        return save(persona);
+    }
+    
+    public boolean update(Integer index){
+        return update(persona,index);
+    }
+    
+    
+    //Getter and Setter
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public LinkedList<Persona> getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(LinkedList<Persona> personas) {
+        this.personas = personas;
+    }
+    
+    
+   
+    
+    
     
 }
