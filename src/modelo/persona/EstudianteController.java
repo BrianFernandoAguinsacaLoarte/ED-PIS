a4601fb7cf16dfb8034e760ccf2073c2da84fcca
Brian Fernando <brian.aguinsaca@unl.edu.ec>
2023-12-26 19:45:12 -0500

    Creación y agregando métodos a la clase EstudianteController

--- src/modelo/persona/EstudianteController.java
+++ src/modelo/persona/EstudianteController.java
@@ -4,10 +4,52 @@
  */
 package modelo.persona;
 
+import controlador.dao.DataAccessObject;
+import modelo.Estudiante;
+import controlador.TDA.listas.LinkedList;
+
+
+
 /**
  *
  * @author Usuario iTC
  */
-public class EstudianteController {
+public class EstudianteController extends DataAccessObject<Estudiante>{
+    
+    //Objetos
+    private Estudiante estudiante = new Estudiante();
+    private LinkedList<Estudiante> estudiantes = new LinkedList<>();
+    
+    //Constructor
+    public EstudianteController() {
+        super(Estudiante.class);
+    }
+    
+    //Métodos
+    public boolean save(){
+        estudiante.setId(generated_id());//BDD Esto desaparece
+        return save(estudiante);
+    }
+    
+    public boolean update(Integer index){
+        return update(estudiante,index);
+    }
+    
+    //Getter and Setter
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public LinkedList<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(LinkedList<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
     
 }
