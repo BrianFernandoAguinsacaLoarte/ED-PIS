d6a34a856e6379297aba62266d3e75315694316b
SebasISG <leoare0412@gmail.com>
2023-12-28 11:56:53 -0500

    actu
    actu

---
+++ src/controlador/periodos/PeriodoControlador.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador.periodos;
+
+import controlador.Excepcion.VacioExcepcion;
+import controlador.TDA.listas.LinkedList;
+import controlador.dao.DataAccessObject;
+
+import modelo.PeriodoAcademico;
+
+/**
+ *
+ * @author Usuario 1
+ */
+public class PeriodoControlador extends DataAccessObject<PeriodoAcademico> {
+
+    //Objetos
+    private Estudiante estudiante = new Estudiante();
+    private LinkedList<Estudiante> estudiantes = new LinkedList<>();
+    private Integer index = -1; 
+    
+    //Constructor
+    public EstudianteController() {
+        super(Estudiante.class);
+    }
+    
+    //Métodos
+    public boolean save(){
+        estudiante.setId(generated_id());//BDD Esto desaparece
+        return save(estudiante);
+    }
+    
+    public boolean update(Integer index){
+        return update(estudiante,index);
+    }
+    
+    //Getter and Setter
+    public Estudiante getEstudiante() {
+        
+        if(estudiante == null){
+            estudiante = new Estudiante();
+        }
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public LinkedList<Estudiante> getEstudiantes() {
+
+        if(estudiantes.isEmpty()){
+            estudiantes = listAll();
+        }
+        return estudiantes;
+    }
+
+    public void setEstudiantes(LinkedList<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+    
+}
