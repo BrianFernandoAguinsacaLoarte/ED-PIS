a8eaf824723c60ff86fff2cc28eaefc9e9f5cc98
JhostinJATM <jhos_tap@hotmail.com>
2023-12-31 21:39:38 -0500

    Actualizacion Clase Tarea
    Modificacion y agregamiento de atributos

--- src/modelo/Tarea.java
+++ src/modelo/Tarea.java
@@ -4,6 +4,7 @@
  */
 package modelo;
 
+import java.sql.Blob;
 import java.util.Date;
 
 /**
@@ -12,61 +13,73 @@ import java.util.Date;
  */
 public class Tarea {
     
-    private Integer id;
-    private String tema;
-    private Date fechaCreacion;
-    private Date fechaEntrega;
-    private String archivo;
+    private int codigopdf;
+    private String nombrepdf;
+    private String fechaCreacion;
+    private String fechaEntrega;
+    private String descripcion;
+    private Blob archivopdf;
+    private String extensionArchivo;
 
     public Tarea() {
     }
 
-    public Tarea(Integer id, String tema, Date fechaCreacion, Date fechaEntrega, String archivo) {
-        this.id = id;
-        this.tema = tema;
-        this.fechaCreacion = fechaCreacion;
-        this.fechaEntrega = fechaEntrega;
-        this.archivo = archivo;
+    public int getCodigopdf() {
+        return codigopdf;
     }
 
-    public Integer getId() {
-        return id;
+    public void setCodigopdf(int codigopdf) {
+        this.codigopdf = codigopdf;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public String getNombrepdf() {
+        return nombrepdf;
     }
 
-    public String getTema() {
-        return tema;
+    public void setNombrepdf(String nombrepdf) {
+        this.nombrepdf = nombrepdf;
     }
 
-    public void setTema(String tema) {
-        this.tema = tema;
-    }
-
-    public Date getFechaCreacion() {
+    public String getFechaCreacion() {
         return fechaCreacion;
     }
 
-    public void setFechaCreacion(Date fechaCreacion) {
+    public void setFechaCreacion(String fechaCreacion) {
         this.fechaCreacion = fechaCreacion;
     }
 
-    public Date getFechaEntrega() {
+    public String getFechaEntrega() {
         return fechaEntrega;
     }
 
-    public void setFechaEntrega(Date fechaEntrega) {
+    public void setFechaEntrega(String fechaEntrega) {
         this.fechaEntrega = fechaEntrega;
     }
 
-    public String getArchivo() {
-        return archivo;
+    public String getDescripcion() {
+        return descripcion;
     }
 
-    public void setArchivo(String archivo) {
-        this.archivo = archivo;
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
+
+    public Blob getArchivopdf() {
+        return archivopdf;
+    }
+
+    public void setArchivopdf(Blob archivopdf) {
+        this.archivopdf = archivopdf;
+    }
+
+    public String getExtensionArchivo() {
+        return extensionArchivo;
+    }
+
+    public void setExtensionArchivo(String extensionArchivo) {
+        this.extensionArchivo = extensionArchivo;
+    }
+
+    
     
 }
