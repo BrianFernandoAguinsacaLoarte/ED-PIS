6f6cf60e2d51d43f0195b750a7004bdac88a7ce8
Brian Fernando <brian.aguinsaca@unl.edu.ec>
2024-01-02 10:06:32 -0500

    Incluyendo id a la clase DocenteMateria

--- src/modelo/DocenteMateria.java
+++ src/modelo/DocenteMateria.java
@@ -10,4 +10,55 @@ package modelo;
  */
 public class DocenteMateria {
     
+    //Atributos
+    private Integer id;
+    private Integer id_Docente;
+    private Integer id_Materia;
+    private Integer id_PeriodoAcademico;
+    
+    //Constructor
+
+    public DocenteMateria(Integer id, Integer id_Docente, Integer id_Materia, Integer id_PeriodoAcademico) {
+        this.id = id;
+        this.id_Docente = id_Docente;
+        this.id_Materia = id_Materia;
+        this.id_PeriodoAcademico = id_PeriodoAcademico;
+    }
+    
+    //Getter and Setter
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId_Docente() {
+        return id_Docente;
+    }
+
+    public void setId_Docente(Integer id_Docente) {
+        this.id_Docente = id_Docente;
+    }
+
+    public Integer getId_Materia() {
+        return id_Materia;
+    }
+
+    public void setId_Materia(Integer id_Materia) {
+        this.id_Materia = id_Materia;
+    }
+
+    public Integer getId_PeriodoAcademico() {
+        return id_PeriodoAcademico;
+    }
+
+    public void setId_PeriodoAcademico(Integer id_PeriodoAcademico) {
+        this.id_PeriodoAcademico = id_PeriodoAcademico;
+    }
+    
+    
+    
 }
